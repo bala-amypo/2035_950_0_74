92e7a8370d86d578142976e49ff0843b764b9684
bala-amypo <bala@amypo.in>
2025-12-17 08:34:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/student.java
+++ src/main/java/com/example/demo/entity/student.java
@@ -1,31 +1,47 @@
-import jakartra.persistence.*;
-public class student{
+import jakarta.persistence.*;
+
+@Entity
+public class student {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
     private String email;
-    public Integer getId(){
+
+  
+    public student() {
+    }
+
+    public student(Integer id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
         return id;
     }
-    public void setId(Integer id){
-        this.id=id;
+
+    public void setId(Integer id) {
+        this.id = id;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public void setName(String name){
-        this.name=name;
+
+    public void setName(String name) {
+        this.name = name;
     }
-    public String getEmail(String email){
+
+
+    public String getEmail() {
         return email;
     }
-    public void setEmail(String email){
-        this.email=email;
-    }    
-    public student(Integer id,String name,String email){
-        this.id=id;
-        this.name=name;
-        this.email=email;
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
